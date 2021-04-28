337e664ffc056e4b313d03e82500a5abeb505064
dario199914 <dario.diazmoreta.14@gmail.com>
2021-04-28 11:46:51 -0500

    migraciones

---
+++ database/migrations/2021_04_28_162436_create_tbl_producto_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTblProductoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_producto', function (Blueprint $table) {
+            $table->id('pro_id');
+            $table->integer('pro_codigo');
+            $table->string('pro_nombre',100);
+            $table->integer('pro_stock');
+            $table->string('pro_des',100);
+            $table->double('pro_precio',4,2);
+            $table->string('pro_obser',100);
+            $table->timestamp('pro_fecha');
+            $table->double('pro_ganancia',4,2);
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_producto');
+    }
+}
