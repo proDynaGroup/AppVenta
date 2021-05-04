5e882a7f79147416d3a8ed472438978000b682b4
dario199914 <dario.diazmoreta.14@gmail.com>
2021-05-04 08:44:03 -0500

    migraciones 1.5

--- config/livewire.php
+++ config/livewire.php
@@ -55,7 +55,7 @@ return [
     */
 
 
-    'asset_url' => 'http://localhost/AppVenta/pub
+    'asset_url' => 'http://localhost/AppVenta/public',
  
 
     /*
--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,7 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('ejemplo',function(){
+return view('ejemplo');
+});
